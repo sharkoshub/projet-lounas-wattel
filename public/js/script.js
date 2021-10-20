a6d92e0c1e4e12f47c41ed25b301251a1b91d8ff
sharkoshub <watcoco59@gmail.com>
2021-10-20 14:34:29 +0100

    feat: barre de recherche

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,6 @@
-// Import the functions you need from the SDKs you need
+alert('hello');
+
+/*// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,3 +21,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+*/
